701c712447f7150cea168d9034156e28215a0b04
sampan-mangosciences <sampan.verma@mangosciences.com>
2022-01-24 18:00:39 +0530

    w

--- client/src/react_magic/blog_create.js
+++ client/src/react_magic/blog_create.js
@@ -1,27 +1,53 @@
-import React, { useState, useEffect } from "react";
-import Editor from "./editor";
-export default function Blog_create() {
-    const [editorLoaded, setEditorLoaded] = useState(false);
-    const [data, setData] = useState("");
-  
-    useEffect(() => {
-      setEditorLoaded(true);
-    }, []);
-  
-    return (
-      <div className="App">
-        <h1>ckEditor 5</h1>
-  
-        <Editor
-          name="description"
-          onChange={(data) => {
-             setData(data);
-           
-          }}
-          editorLoaded={editorLoaded}
-        />
-  
-        {JSON.stringify(data)}
-      </div>
-    );
-  }
+import React, { Component, useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+ 
+ const Blog_create=()=>{
+const [data,setdata] =  useState("")
+ClassicEditor.builtinPlugins.map( plugin => plugin.pluginName );
+        return (
+            <div className="App">
+               
+                <CKEditor
+               
+                    editor={ ClassicEditor }
+                     
+                    config={{
+                        
+                        toolbar: {
+                            items: [
+                                'heading', '|',
+                                'alignment', '|',
+                                'bold', 'italic', 'strikethrough', 'underline', 'subscript', 'superscript', '|',
+                                'link', '|',
+                                'bulletedList', 'numberedList', 'todoList',
+                              
+                                'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor', '|',
+                                'code', 'codeBlock', '|',
+                                'insertTable', '|',
+                                'outdent', 'indent', '|',
+                                'uploadImage', 'blockQuote', '|',
+                                'undo', 'redo'
+                            ],
+                            shouldNotGroupWhenFull: true
+                        },
+                        
+                        ckfinder:{
+                        uploadUrl:'http://localhost:8000/image/upload_image'
+                    }}}
+                    
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        setdata(data)
+                        console.log(data)
+                    } }
+ 
+                />
+                
+              
+                <div>{data}</div>
+            </div>
+
+        );
+    }
+export default Blog_create;
--- client/src/react_magic/editor.js
+++ client/src/react_magic/editor.js
@@ -1,36 +1,43 @@
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
  
-function Editor({ onChange, editorLoaded, name, value }) {
-  const editorRef = useRef();
-  const { CKEditor, ClassicEditor } = editorRef.current || {};
+// function Editor({ onChange, editorLoaded, name, value }) {
+//   const editorRef = useRef();
+//   const { CKEditor, ClassicEditor } = editorRef.current || {};
 
-  useEffect(() => {
-    editorRef.current = {
-      CKEditor: require("@ckeditor/ckeditor5-react").CKEditor,  
-      ClassicEditor: require("@ckeditor/ckeditor5-build-classic"),
-     
-    };
-  }, []);
+//   useEffect(() => {
+//     editorRef.current = {
+//       CKEditor: require("@ckeditor/ckeditor5-react").CKEditor,  
+//       ClassicEditor: require("@ckeditor/ckeditor5-build-classic")
+//     };
+//   }, []);
  
-  return (
-    <div>
-      {editorLoaded ? (
-        <CKEditor
-          type=""
-          name={name}
-          editor={ClassicEditor}
-          data={value}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            // console.log({ event, editor, data })
-            onChange(data);
-          }}
-        />
-      ) : (
-        <div>Editor loading</div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {editorLoaded ? (
+//         <CKEditor
+//           type=""
+//           name={name}
+//           editor={ClassicEditor}
+//           data={value}
+//           config={ {
+//             plugins: [ CKFinder],
+//             toolbar: [ 'imageUpload' ],
+//             ckfinder: {
+//                 uploadUrl: 'https://example.com/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images&responseType=json'
+//             }
+//         } }
 
-export default Editor;
+//           onChange={(event, editor) => {
+//             const data = editor.getData();
+//             // console.log({ event, editor, data })
+//             onChange(data);
+//           }}
+//         />
+//       ) : (
+//         <div>Editor loading</div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Editor;
